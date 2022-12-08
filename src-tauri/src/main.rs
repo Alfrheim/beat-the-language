0a59f20e66b0ac378dd6fe98bae8951fbf4f11c8
Alfrheim <1399702+Alfrheim@users.noreply.github.com>
2022-12-08 02:42:17 +0100

    we get the random english word from rust now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,15 @@
 
 fn main() {
   tauri::Builder::default()
+      .invoke_handler(tauri::generate_handler![get_random_word])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_random_word(language: &str) -> String {
+    match language {
+        "EN" => String::from("House"),
+        _ => String::from("couldn't recover the word")
+    }
 }
--- src/spanish/Spanish.js
+++ src/spanish/Spanish.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./spanish.css";
 import CardComponent from "./CardComponent";
 import Score from "./Score";
 import Word from "./Word";
+import { invoke } from '@tauri-apps/api';
+
+function GetEnglishRandomWord() {
+    const [randomWord, setWord] = useState('')
+
+    function getRandomWord() {
+        invoke('get_random_word', {language: "EN"}).then((message) => setWord(message))
+    }
+    useEffect(getRandomWord, [])
+
+    return {randomWord}
+}
 
 function Spanish() {
+    const {randomWord} = GetEnglishRandomWord()
     return (
         <div className="cards">
             <Score />
-            <Word word='House'/>
+            <Word word={randomWord}/>
             <CardComponent />
             {/*// <!-- footer -->*/}
         </div>
