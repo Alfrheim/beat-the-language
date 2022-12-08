4e56f55fb1e96ff3dc73e69aa3e8f63909995f72
Alfrheim <1399702+Alfrheim@users.noreply.github.com>
2022-12-08 23:36:48 +0100

    changing cards to something less fancy but with more sense for now

--- src/spanish/Card.js
+++ src/spanish/Card.js
@@ -2,16 +2,9 @@ import React from 'react';
 
 const Card = ({ word, onClick }) => {
     return (
-        <div className="card">
-            <div className="face face1">
-                <div className="content">
-                    <h3>{word}</h3>
-                </div>
-            </div>
-            <div className="face face2">
-                <div className="content">
-                    <button type="button" onClick={onClick}>Read More</button>
-                </div>
+        <div className="game-card">
+            <div className="game-card2">
+                <h3>{word}</h3>
             </div>
         </div>
     );
