80b7c407168ba2b41b68ea44300a2a894e69fc8f
Alfrheim <1399702+Alfrheim@users.noreply.github.com>
2023-01-17 20:44:16 +0100

    we have something like cards to select words and verbs, text still not aligned and cards not centered

--- src/LanguagesMenu.js
+++ src/LanguagesMenu.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import './language-menu.css';
 
 function LanguagesMenu() {
 
-    return (
-        <div className="container">
-            <Link to="/spanish" state={{cmd:'get_word'}}>Learn spanish words</Link>
-            <Link to="/spanish" state={{cmd:'get_verb'}}>Learn spanish words</Link>
-        </div>
-    );
+  return (
+    <div className="container">
+      <Link to="/spanish" state={{ cmd: 'get_word' }}><button className="button menu" type="button"><h3>WORDS</h3></button></Link>
+      <Link to="/spanish" state={{ cmd: 'get_verb' }}><button className="button menu" type="button"><h3>VERBS</h3></button></Link>
+    </div>
+  );
 }
 
 export default LanguagesMenu;
